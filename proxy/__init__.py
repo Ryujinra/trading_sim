ad7a3a0b55b2a0ae66a8fb3583fd977f9b908794
kyhorne <me@kyhorne.com>
2019-02-28 20:57:31 -0500

    Added logger

---
+++ proxy/__init__.py
@@ -0,0 +1,12 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s:%(funcName)s:%(message)s')
+file_handler = logging.StreamHandler()
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+if __name__ == '__main__':
+    logger.info('Hello, world!')

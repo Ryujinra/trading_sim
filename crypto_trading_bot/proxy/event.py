81fd55e828db049743589ad66c32dab06a07a583
kyhorne <me@kyhorne.com>
2019-03-10 15:02:26 -0400

    Implemented test strategy

--- crypto_trading_bot/proxy/event.py
+++ crypto_trading_bot/proxy/event.py
@@ -5,22 +5,19 @@ from enum import Enum
 
 class EventType(Enum):
 
-    REGISTER_STRATEGY = 'REGISTER_STRATEGY'
-    NEW_CANDLESTICK = 'NEW_CANDLESTICK'
+    REGISTER_TEST_STRATEGY = 'REGISTER_TEST_STRATEGY'
+    NEW_TEST_CANDLESTICK = 'NEW_TEST_CANDLESTICK'
 
 
 class Event(object):
 
-    def __init__(self, type, payload, conn, addr):
+    def __init__(self, type, payload, conn):
         self.type = type
         self.payload = payload
         self.conn = conn
-        self.addr = addr
 
-    def get_conn_and_addr(self):
-        return (self.conn, self.addr)
+    def get_conn(self):
+        return self.conn
 
     def to_json(self):
-        return json.loads(json.dumps({
-            'type': self.type,
-            'payload': self.payload}))
+        return json.loads(json.dumps({'type': self.type, 'payload': self.payload}))
--- crypto_trading_bot/proxy/proxy.py
+++ crypto_trading_bot/proxy/proxy.py
@@ -7,6 +7,24 @@ from .subscribable import Handler
 from .strategy import Strategy
 from .event import Event, EventType
 from util.logger import logger
+from exchange.exchange_db import ExchangeDatabase
+
+
+class Connection(object):
+
+    def __init__(self, socket, addr):
+        self.socket = socket
+        self.addr = addr
+
+    def get_conn(self):
+        return (self.socket, self.addr)
+
+    def __str__(self):
+        ip, port = self.addr
+        return 'from: {}:{}'.format(ip, port)
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class Proxy(Handler):
@@ -17,60 +35,52 @@ class Proxy(Handler):
 
     def __init__(self):
         self.strategies = {}
+        self.exchange_db = ExchangeDatabase()
         proxy = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         proxy.bind((self.HOST, self.PORT))
+        logger.info('Listening...')
         while True:
             proxy.listen()
-            conn, addr = proxy.accept()
-            ip, port = addr
-            logger.info('New connection: {}:{}'.format(ip, port))
-            threading.Thread(target=self.listener, args=(conn, addr)).start()
+            _socket, addr = proxy.accept()
+            conn = Connection(_socket, addr)
+            logger.info('New connection {}'.format(conn))
+            threading.Thread(target=self.listener, args=(conn,)).start()
 
-    def listener(self, conn, addr):
-        ip, port = addr
+    def get_db(self):
+        return self.exchange_db
+
+    def listener(self, conn):
+        socket, addr = conn.get_conn()
         while True:
-            msg = conn.recv(self.BUFFER_SIZE)
+            msg = socket.recv(self.BUFFER_SIZE)
             try:
                 msg = json.loads(msg)
             except ValueError:
-                logger.info('Message from: {}:{}: is not parsable to '
-                            'json'.format(ip, port))
+                logger.info('Message {}: is not parsable to json'.format(conn))
                 continue
             if all(key in msg for key in ('type', 'payload')):
-                self.handler(Event(msg['type'], msg['payload'], conn, addr))
+                self.handler(Event(msg['type'], msg['payload'], conn))
             else:
-                logger.info(
-                    'Message from: {}:{}: does not contain type or '
-                    'payload'.format(ip, port))
+                logger.info('Message {}: does not contain a type or payload'.format(conn))
 
     def handler(self, event):
         msg = event.to_json()
-        conn, addr = event.get_conn_and_addr()
-        ip, port = addr
-        logger.info('Message type: {}: from: {}:{}'.format(
-            msg['type'], ip, port))
-        if msg['type'] == EventType.REGISTER_STRATEGY.name:
-            if any(key not in msg['payload'] for key in ('exchange',
-                                                         'currencyPair',
-                                                         'amount', 'interval')):
-                logger.info('{}: from {}:{}: message does not contain '
-                            'exchange, currency pair, interval or ' 'amount'.format(
-                                EventType.REGISTER_STRATEGY, ip, port))
+        conn = event.get_conn()
+        logger.info('Message type: {}: {}'.format(msg['type'], conn))
+        if msg['type'] == EventType.REGISTER_TEST_STRATEGY.name:
+            if conn.get_conn() in self.strategies:
+                logger.info('Strategy already exist {}: overriding the current strategy'.format(conn))
+                self.strategies[conn.get_conn()].running = False
+            self.strategies[conn.get_conn()] = Strategy(self, conn, msg['payload'])
+            self.strategies[conn.get_conn()].start()
+        elif msg['type'] == EventType.NEW_TEST_CANDLESTICK.name:
+            if conn.get_conn() not in self.strategies:
+                logger.info("Connection {}: is not a registered strategy".format(conn))
                 return
-            # TODO: Check if the request contains a valid currency pair
-            # TODO: Check if the request contains a valid amount
-            # TODO: Check if the request contains a valid interval
-            # TODO: Forfeit the balance of the current strategy if it is
-            # overwritten
-            if (conn, addr) in self.strategies:
-                logger.info(
-                    'Strategy already exist from: {}:{}: overriding the '
-                    'current strategy'.format(ip, port))
-                self.strategies[(conn, addr)].running = False
-            self.strategies[(conn, addr)] = Strategy(
-                self, msg['payload'], conn, addr)
-        elif msg['type'] == EventType.NEW_CANDLESTICK.name:
-            pass
+            payload = msg['payload']
+            high, low, open, close = self.exchange_db.get_chart_data(payload['exchange'], payload['pair'], payload['period'], payload['date'])
+            socket, addr = conn.get_conn()
+            data = {'high': high, 'low': low, 'open': open, 'close': close}
+            socket.send(str(data).encode())
         else:
-            logger.info('Invalid message type: {}: from: {}:{}'.format(
-                msg['type'], ip, port))
+            logger.info('Invalid message type: {}: {}'.format(msg['type'], conn))
